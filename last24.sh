0090f81f5614f8a68bf891506cdcd9ab4cc6dd57
Genpurpose Sean <sshilton@genpurpose.ddns.net>
2019-12-28 23:06:48 -0500

    changed email destination

--- last24.sh
+++ last24.sh
@@ -1,7 +1,7 @@
 #! /bin/sh
 #set -x
-#email=sean.shilton@hackensackmeridian.org
-email=sshilton@outlook.com
+email=sean.shilton@hackensackmeridian.org,sshilton@outlook.com
+#email=sshilton@outlook.com
 alerts="/tmp/alerts.html"
 avail="/tmp/avail.html"
 wget -q  --user=epic --password=epic -O $avail "http://genpurpose.ddns.net/cgi-bin/icinga/avail.cgi?show_log_entries=&hostgroup=HMHN&timeperiod=last24hours&smon=12&sday=1&syear=2019&shour=0&smin=0&ssec=0&emon=12&eday=28&eyear=2019&ehour=24&emin=0&esec=0&rpttimeperiod=&assumeinitialstates=yes&assumestateretention=yes&assumestatesduringnotrunning=yes&includesoftstates=no&initialassumedhoststate=0&initialassumedservicestate=0&backtrack=4&content_type=html"
