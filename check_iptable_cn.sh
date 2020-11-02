e52cc176b7bcc0676ee2a465dbf82ed4a04cba90
Ubuntu <sshilton@genpurpose.ddns.net>
2020-11-02 15:12:03 -0500

    fixed formattting so we can get a number with firewall rules

--- check_iptable_cn.sh
+++ check_iptable_cn.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
-set -x
+#set -x
 count=`sudo iptables -L -nvx | grep "cn Country Drop" | wc -l`
-echo $count
+#echo $count
 	if [ $count -gt 0 ];then
-		echo "OK: iptables has $count rules for blocking China"
+		echo "OK: iptables has $count rules for blocking China|china=$count"
 		exit 0
 	else
 		echo "CRITICAL: iptables has $count rules for China"
